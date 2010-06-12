ec07ec7deda08375d8122dd50e35ff1fd751343f
Tom Allen <tom@telega.org>
2010-06-12 12:38:23 +0100

    Added function to print last refresh time; separated out JSON and URL functions.

--- FeverBuddy.py
+++ FeverBuddy.py
@@ -12,6 +12,7 @@ import getopt
 import json
 import hashlib
 import urllib
+import time
 
 
 help_message = '''
@@ -19,10 +20,12 @@ This is for accessing the Fever API (http://www.feedafever.com/api)
 Currently it is only useful for retrieving Hot Items
 
 Options:
--s, --server: specify the location of a fever installation  [required] 
--e, --email: specify the email associated with the installation [required]
--p, --password: specify the password [required]
--d, --days: specify days value [required]
+-s, --server        : specify the location of a fever installation  [required] 
+-e, --email         : specify the email associated with the installation [required]
+-p, --password      : specify the password [required]
+-d, --days          : specify days value
+-i                  : print hot items
+-r                  : print last refresh (minutes ago)
 
 Example Usage:
 python FeverBuddy.py -s mydomain.com/fever -e me@mydomain.com -p password -d 2 -i
@@ -73,14 +76,22 @@ class FeverBuddy(Exception):
         
     def getDays(self):
         return self.days
+        
+    def setUrl(self):
+        self.url = str(self.getEndpoint())+str(self.getParams())
+    
+    def getUrl(self):
+        return self.url
+        
+    def getJson(self):
+        self.feverJson = json.load(urllib.urlopen(self.getUrl(),urllib.urlencode(self.getApiKey())))
+        return self.feverJson   
     
     def getHotItems(self):
         "get Hot Items from fever, returns a list"
         storyque = []
         
-        url = str(self.getEndpoint())+str(self.getParams())
-
-        myJson = json.load(urllib.urlopen(url,urllib.urlencode(self.getApiKey())))
+        myJson = self.getJson()
 
         x = 0
 
@@ -106,17 +117,40 @@ class FeverBuddy(Exception):
         for item in storyque:
             print item.encode('utf8')
 
+    
+    def getLastRefresh(self):
+        myJson = self.getJson()
+        
+        #time of last refresh
+        rtime = time.localtime(int(myJson["last_refreshed_on_time"]))
+        #current time
+        ntime = time.localtime()
+        #figure out roughly how many minutes ago
+        self.lastRefresh = ((ntime[0]-rtime[0])*525948) + ((ntime[3]-rtime[3])*60) +(ntime[4]-rtime[4])
+        
+        return self.lastRefresh
+    
+    def printLastRefresh(self):
+        print "Last Refresh: %d minutes ago" %self.getLastRefresh()
+        
 
 def main(argv=None):
     if argv is None:
         argv = sys.argv
+    
+    
+    #default values for some options
+    verbose = False
+    hotitems = False
+    lastrefresh = False
+    
     
     #create a fb item
     fb = FeverBuddy("FeverBuddy")
     
     try:
         try:
-            opts, args = getopt.getopt(argv[1:], "ho:s:e:p:d:vi", ["help", "output=","server=","email=","password=","days="])
+            opts, args = getopt.getopt(argv[1:], "ho:s:e:p:d:vir", ["help", "output=","server=","email=","password=","days="])
         except getopt.error, msg:
             raise Usage(msg)
     
@@ -126,6 +160,8 @@ def main(argv=None):
                 verbose = True
             elif option == "-i":
                 hotitems = True
+            elif option == "-r":
+                lastrefresh = True
             elif option in ("-h", "--help"):
                 raise FeverBuddy(help_message)
             elif option in ("-o", "--output"):
@@ -147,7 +183,12 @@ def main(argv=None):
         
         if hotitems == True:
             fb.setParams("&links&offset=0&range=",fb.getDays(),"&page=1")
+            fb.setUrl()
             fb.printHotItems(fb.getHotItems())
+        elif lastrefresh == True:
+            fb.setParams("")
+            fb.setUrl()
+            fb.printLastRefresh()
         
     except FeverBuddy, err:
         print >> sys.stderr, sys.argv[0].split("/")[-1] + ": " + str(err.msg)
--- README.txt
+++ README.txt
@@ -8,10 +8,13 @@ This is for accessing the Fever API (http://www.feedafever.com/api)
 Currently it is only useful for retrieving Hot Items
 
 Options:
--s, --server: specify the location of a fever installation  [required] 
--e, --email: specify the email associated with the installation [required]
--p, --password: specify the password [required]
--d, --days: specify days value [required]
+-s, --server        : specify the location of a fever installation  [required] 
+-e, --email         : specify the email associated with the installation [required]
+-p, --password      : specify the password [required]
+-d, --days          : specify days value
+-i                  : print hot items
+-r                  : print last refresh (minutes ago)
 
 Example Usage:
 python FeverBuddy.py -s mydomain.com/fever -e me@mydomain.com -p password -d 2 -i
+'''
